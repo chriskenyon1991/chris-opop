360e909eceed82749b3cf82cfee4e78a2c4a7ea5
christopher kenyon <c_kenyon1991@outlook.com>
2023-03-08 10:19:01 +0000

    update lambda

--- hello-world/app.js
+++ hello-world/app.js
@@ -2,21 +2,21 @@ let response;
 
 exports.lambdaHandler = async (event, context) => {
   try {
-    response = {
-      statusCode: 200,
-      body: JSON.stringify({
-        message: "============================================== pipe works",
-      }),
-    };
-    // var eventText = JSON.stringify(event, null, 2);
-    // console.log("Received event:", eventText);
-    // var sns = new AWS.SNS();
-    // var params = {
-    //   Message: eventText,
-    //   Subject: "Test SNS From Lambda",
-    //   TopicArn:
-    //     "arn:aws:sns:eu-west-2:925221024030:opop-pipeline-Gamma-opopSNS-GX3dgGmSrBVw",
+    // response = {
+    //   statusCode: 200,
+    //   body: JSON.stringify({
+    //     message: "pipeline works",
+    //   }),
     // };
+    var eventText = JSON.stringify(event, null, 2);
+    console.log("Received event:", eventText);
+    var sns = new AWS.SNS();
+    var params = {
+      Message: eventText,
+      Subject: "Test SNS From Lambda",
+      TopicArn:
+        "arn:aws:sns:eu-west-2:925221024030:opop-pipeline-Gamma-opopSNS-GX3dgGmSrBVw",
+    };
     sns.publish(params, context.done);
   } catch (err) {
     console.log(err);
